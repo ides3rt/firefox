45adc3608fcb867e4214ee84a4ca82d1e8fb029c
ides3rt <ides3rt@tutanota.com>
2022-01-09 17:58:29 +0000

    Updated

--- src/user.js
+++ src/user.js
@@ -1,5 +1,4 @@
 user_pref("fission.autostart", true);
-user_pref("browser.startup.page", 0);
 user_pref("javascript.use_us_english_locale", true);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.server", "data:,");
